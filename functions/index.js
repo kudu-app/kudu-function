8935b28002f86980b4aa4758397c0c0f63ab07ce
Randy Cahyana <ranren.cahyana@gmail.com>
2017-04-12 22:01:52 +0700

    Update item ref.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 
-const itemRef = "/data/{userId}/items/{itemId}"
+const itemRef = "/data/{userId}/items/{date}/{itemId}"
 const TMP_DIR = '/tmp/';
 
 /**
